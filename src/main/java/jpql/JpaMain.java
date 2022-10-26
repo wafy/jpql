119aea4ceb66cc8a01a041a503eb5d6a1d0140a9
최선혁 <wafy75@gmail.com>
2022-10-26 22:30:38 +0900

    coalesce 구문

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,7 +19,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername(null);
             member.setAge(10);
             member.setTeam(team);
             member.setType(MemberType.ADMIN);
@@ -29,11 +29,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select " +
-                    "case when m.age <=10 then '학생요금' " +
-                    "     when m.age >=60 then '경로요금' " +
-                    "       else  '일반요금' end " +
-                    " from Member m ";
+            String query = "select  coalesce(m.username, '이름 없는 회원') from Member m";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
             for (String s : result) {
