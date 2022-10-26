61a33d0b334aacf57cc3438f8a22dde10f039097
최선혁 <wafy75@gmail.com>
2022-10-26 16:09:47 +0900

    프로젝를 위한 기본 엔티티 추가

---
+++ src/main/java/jpql/Member.java
@@ -0,0 +1,26 @@
+package jpql;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+
+}
