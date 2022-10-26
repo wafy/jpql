cb5a6c96e9a47fbc2c39f15a5d9167a9a9de1703
최선혁 <wafy75@gmail.com>
2022-10-26 22:31:22 +0900

    nullif

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,7 +19,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername(null);
+            member.setUsername("관리자");
             member.setAge(10);
             member.setTeam(team);
             member.setType(MemberType.ADMIN);
@@ -29,7 +29,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select  coalesce(m.username, '이름 없는 회원') from Member m";
+            String query = "select nullif(m.username, '관리자') from Member m";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
             for (String s : result) {
