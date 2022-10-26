d5b4d1285fd82d64c3771ccff9d91623f7f30708
최선혁 <wafy75@gmail.com>
2022-10-26 22:29:30 +0900

    case 구문

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -29,13 +29,16 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', true from Member m " +
-                    "where m.type = :userType";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
+            String query = "select " +
+                    "case when m.age <=10 then '학생요금' " +
+                    "     when m.age >=60 then '경로요금' " +
+                    "       else  '일반요금' end " +
+                    " from Member m ";
+            List<String> result = em.createQuery(query, String.class)
                     .getResultList();
-            System.out.println("result.size() = " + result.size());
-
+            for (String s : result) {
+                System.out.println("s = " + s);
+            }
             tx.commit();
         } finally {
             emf.close();
